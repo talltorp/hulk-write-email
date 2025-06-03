13692bd6f669f42fc75b75f4ee69b81a36b6ec76
Tobias Talltorp <tobias@talltorp.se>
2025-06-03 08:30:30 +0200

    Read FOLDER PATH FROM SECRET

--- config/initializers/hulk_secrets.rb
+++ config/initializers/hulk_secrets.rb
@@ -1,7 +1,7 @@
 require 'hulk'
 
 Hulk.secrets = {
-  hulk_prompt_template: File.read(File.join(Rails.root, '..', 'hulk_prompt_template.txt')),
+  hulk_prompt_template: File.read(File.join(Rails.root, ENV['SECRET_FOLDER_PATH'], 'hulk_prompt_template.txt')),
   first_reference: File.read(File.join(Rails.root, ENV['SECRET_FOLDER_PATH'], 'first_reference.txt')).strip,
   other_references: File.readlines(File.join(Rails.root, ENV['SECRET_FOLDER_PATH'], 'other_references.txt')).map(&:strip)
 }
